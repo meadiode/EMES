51e5546dbd8e90b9f2929a7e43688aea8c66d026
Michael P <meadiode@protonmail.com>
2025-05-31 22:59:01 +0200

    experiments with bitbanged i2c reading/writing

--- src/IS31FL3743A_demo/demo.c
+++ src/IS31FL3743A_demo/demo.c
@@ -14,6 +14,21 @@
 
 #include "badapple.h"
 
+#define BITBANG 1
+
+void bitbang_i2c_write(uint8_t addr, const uint8_t *data, uint32_t size);
+void bitbang_i2c_read(uint8_t addr, uint8_t *data, uint32_t size);
+
+
+#ifdef BITBANG
+# define is31_write(addr, data, size) bitbang_i2c_write(addr, data, size)
+# define is31_read(addr, data, size) bitbang_i2c_read(addr, data, size)
+#else
+# define is31_write(addr, data, size) i2c_write_blocking(i2c0, addr, data, size, false)
+# define is31_read(addr, data, size) i2c_read_blocking(i2c0, addr, data, size, false)
+#endif
+
+
 void is31_set_reg_page(uint8_t page_id)
 {
     uint8_t data[2];
@@ -21,12 +36,12 @@ void is31_set_reg_page(uint8_t page_id)
     /* Unlock the page-select register */
     data[0] = 0xfe;
     data[1] = 0xc5;
-    i2c_write_blocking(i2c0, ADDR, data, 2, false);
+    is31_write(ADDR, data, 2);
 
     /* select page */
     data[0] = 0xfd;
     data[1] = page_id;
-    i2c_write_blocking(i2c0, ADDR, data, 2, false);
+    is31_write(ADDR, data, 2);
 }
 
 void is31_init(void)
@@ -43,17 +58,17 @@ void is31_init(void)
     /* Configuration register */
     data[0] = 0x00;
     data[1] = 0x01;
-    i2c_write_blocking(i2c0, ADDR, data, 2, false);
+    is31_write(ADDR, data, 2);
 
     /* Global Current Control register */
     data[0] = 0x01;
-    data[1] = 0x10;
-    i2c_write_blocking(i2c0, ADDR, data, 2, false);
+    data[1] = 0x70;
+    is31_write(ADDR, data, 2);
 
     /* Pull up/down register config register */
     data[0] = 0x01;
     data[1] = 0x70;
-    i2c_write_blocking(i2c0, ADDR, data, 2, false);
+    is31_write(ADDR, data, 2);
 
 
     is31_set_reg_page(1);
@@ -64,7 +79,7 @@ void is31_init(void)
         data[0] = i;
         data[1] = 0xff;
 
-        i2c_write_blocking(i2c0, ADDR, data, 2, false);
+        is31_write(ADDR, data, 2);
     }
 
     is31_set_reg_page(0);
@@ -75,10 +90,45 @@ void is31_init(void)
         data[0] = i;
         data[1] = 0x00;
 
-        i2c_write_blocking(i2c0, ADDR, data, 2, false);
+        is31_write(ADDR, data, 2);
     }
 }
 
+void is31_printout_config_registers(void)
+{
+
+    uint8_t data = 0xfc;
+    uint8_t rdata = 0;
+
+    is31_write(ADDR, &data, 1);
+    is31_read(ADDR, &rdata, 1);
+
+    printf("Device id: 0x%02X\n", rdata);
+
+    is31_set_reg_page(2);
+
+    const char* const regnames[] =
+    {
+        "Configureation register                 ",
+        "Global Current Control register         ",
+        "Pull Down/Up resistor selection register",
+    };
+
+    for (uint8_t reg_id = 0; reg_id < 3; reg_id++)
+    {
+        
+        if (reg_id == 2)
+        {
+            gpio_put(18, 1);
+        }
+        is31_write(ADDR, &reg_id, 1);
+        is31_read(ADDR, &rdata, 1);
+        
+        gpio_put(18, 0);
+
+        printf("%s : 0x%02X\n", regnames[reg_id], rdata);
+    }
+}
 
 void is31_set_pixel(uint8_t x, uint8_t y, uint8_t val)
 {
@@ -87,7 +137,119 @@ void is31_set_pixel(uint8_t x, uint8_t y, uint8_t val)
     data[0] = 1 + y * 18 + x; /* PWM reg id */
     data[1] = val;
 
-    i2c_write_blocking(i2c0, ADDR, data, 2, false);
+    is31_write(ADDR, data, 2);
+}
+
+void bitbang_i2c_write(uint8_t addr, const uint8_t *data, uint32_t size)
+{
+    /* start condition */
+    gpio_set_dir(PIN_SDA, false);
+    gpio_set_dir(PIN_SCL, false);
+    sleep_us(1);
+    gpio_set_dir(PIN_SDA, true);
+    sleep_us(1);
+
+    for (uint32_t i = 0; i <= size; i++)
+    {
+        uint8_t byte;
+        if (i == 0)
+        {
+            byte = addr << 1;         
+        }
+        else
+        {
+            byte = data[i - 1];
+        }
+
+        for (uint8_t j = 0; j < 8; j++)
+        {
+            gpio_set_dir(PIN_SDA, (byte & 0b10000000) == 0);
+            gpio_set_dir(PIN_SCL, true);
+            sleep_us(1);
+            gpio_set_dir(PIN_SCL, false);
+            sleep_us(1);
+            byte <<= 1;
+        }
+
+        /* ACK cycle */
+        gpio_set_dir(PIN_SDA, true);
+        gpio_set_dir(PIN_SCL, true);
+        sleep_us(1);
+        gpio_set_dir(PIN_SCL, false);
+        sleep_us(1);
+        gpio_set_dir(PIN_SDA, false);
+    }
+
+    /* stop condition */
+    gpio_set_dir(PIN_SDA, true);
+    gpio_set_dir(PIN_SCL, true);
+    sleep_us(1);
+    gpio_set_dir(PIN_SCL, false);
+    gpio_set_dir(PIN_SDA, true);
+    sleep_us(1);
+}
+
+
+void bitbang_i2c_read(uint8_t addr, uint8_t *data, uint32_t size)
+{
+    /* start condition */
+    gpio_set_dir(PIN_SDA, false);
+    gpio_set_dir(PIN_SCL, false);
+    sleep_us(1);
+    gpio_set_dir(PIN_SDA, true);
+    gpio_put(PIN_SDA, 0);
+    sleep_us(1);
+
+    uint8_t byte = (addr << 1) | 1;
+
+    for (uint8_t j = 0; j < 8; j++)
+    {
+        gpio_set_dir(PIN_SDA, (byte & 0b10000000) == 0);
+        gpio_set_dir(PIN_SCL, true);
+        sleep_us(1);
+        gpio_set_dir(PIN_SCL, false);
+        sleep_us(1);
+        byte <<= 1;
+    }
+
+    /* ACK cycle */
+    gpio_set_dir(PIN_SCL, true);
+    sleep_us(1);
+    gpio_set_dir(PIN_SCL, false);
+    sleep_us(1);
+    gpio_set_dir(PIN_SDA, false);
+
+    for (uint32_t i = 0; i < size; i++)
+    {
+        uint8_t rbyte = 0;
+        for (uint8_t j = 0; j < 8; j++)
+        {
+            gpio_set_dir(PIN_SCL, true);
+            sleep_us(1);
+            rbyte |= (gpio_get(PIN_SDA) ? 1 : 0) << (7 - j);
+            gpio_set_dir(PIN_SCL, false);
+            sleep_us(1);
+        }
+        data[i] = rbyte;
+    }
+
+    /* ACK cycle */
+    gpio_set_dir(PIN_SDA, true);
+    gpio_put(PIN_SDA, 1);
+    gpio_set_dir(PIN_SCL, true);
+    sleep_us(1);
+    gpio_set_dir(PIN_SCL, false);
+    sleep_us(1);
+    gpio_set_dir(PIN_SDA, false);
+
+    /* stop condition */
+    gpio_set_dir(PIN_SDA, true);
+    gpio_put(PIN_SDA, 0);
+    gpio_set_dir(PIN_SCL, true);
+    sleep_us(1);
+    gpio_set_dir(PIN_SCL, false);
+    sleep_us(1);
+    gpio_set_dir(PIN_SDA, false);
 }
 
 
@@ -95,57 +257,74 @@ int main(void)
 {
     stdio_init_all();
 
+    printf("SW: %u\n", 4);
+    
     gpio_init(PIN_SDB);
     gpio_init(PIN_SDA);
     gpio_init(PIN_SCL);
+    
+    gpio_init(18);
+    gpio_set_dir(18, true);
+    gpio_put(18, 0);
 
-    i2c_init(i2c0, 400 * 1000);
+    i2c_init(i2c0, 100 * 1000);
+
+#ifdef BITBANG
+    gpio_set_dir(PIN_SDA, false);
+    gpio_set_dir(PIN_SCL, false);
+#else
     gpio_set_function(PIN_SDA, GPIO_FUNC_I2C);
     gpio_set_function(PIN_SCL, GPIO_FUNC_I2C);
-    gpio_pull_up(PIN_SDA);
-    gpio_pull_up(PIN_SCL);
+#endif
 
     gpio_set_dir(PIN_SDB, true);
 
     is31_init();
 
-    sleep_ms(3000);
+    for (uint8_t i = 0; i < 3; i++)
+    {
+        printf("Read-out attempt #%d:\n", i);
+        sleep_ms(1000);
+        is31_printout_config_registers();
+        printf("\n\n");
+    }
+
+
+    is31_set_reg_page(0); 
 
     for (;;)
     {
-
-    for (uint32_t fid = 0; fid < 6560; fid++)
-    {
-        const uint8_t *frame = &badapple[38 * fid];
-
-        uint8_t x = 0;
-        uint8_t y = 0;
-
-        for (uint32_t i = 0; i < 38; i++)
+        for (uint32_t fid = 0; fid < 6560; fid++)
         {
-            for (uint8_t j = 0; j < 4; j++)
+            const uint8_t *frame = &badapple[38 * fid];
+
+            uint8_t x = 0;
+            uint8_t y = 0;
+
+            for (uint32_t i = 0; i < 38; i++)
             {
-                uint8_t val = (frame[i] >> (j * 2)) & 0b11;
-                val *= 2;
-                is31_set_pixel(x, y, val);
-                x++;
-
-                if (x >= 15)
+                for (uint8_t j = 0; j < 4; j++)
                 {
-                    y++;
-                    x = 0;
-                }
+                    uint8_t val = (frame[i] >> (j * 2)) & 0b11;
+                    val *= 0x04;
+                    is31_set_pixel(x, y, val);
+                    x++;
 
-                if (y  >= 10)
-                {
-                    break;
+                    if (x >= 15)
+                    {
+                        y++;
+                        x = 0;
+                    }
+
+                    if (y  >= 10)
+                    {
+                        break;
+                    }
                 }
             }
+
+            sleep_ms(20);
         }
-
-        sleep_ms(15);
-    }
-
         sleep_ms(1000);
     }
 
